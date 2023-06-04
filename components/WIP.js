4ea0921659c9dadd23b35cd0f284ae241edc5d43
Alex Cheng <alexcheng0314@gmail.com>
2023-06-04 16:14:17 +0200

    Remove gradiant background

--- components/WIP.js
+++ components/WIP.js
@@ -3,7 +3,7 @@ import logo from "../public/logo.png";
 
 export default function WIP() {
   return (
-    <div className="h-screen flex flex-col justify-center items-center md:flex-row bg-gradient-to-t from-blue-100 via-blue-300 to-blue-500">
+    <div className="h-screen flex flex-col justify-center items-center md:flex-row">
       <Image src={logo} alt="logo" width="500" height="500" className="w-3/4 md:w-fit"/>
       <div className="flex flex-col font-montserrat px-4">
         <h1 className="uppercase text-3xl tracking-widest md:text-3xl">Coming soon..</h1>
